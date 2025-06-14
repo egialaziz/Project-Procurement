5a8b16a8d3a4c7df4eb8812543e1ab4290bca5ea
egialaziz <egi.jack@gmail.com>
2025-06-14 21:12:05 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
-// /app/layout.tsx
-import './globals.css';
-import { ReactNode } from 'react';
-
-export const metadata = {
-  title: 'Procurement Catalogue',
-  description: 'Catalogue system for procurement',
-};
-
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
