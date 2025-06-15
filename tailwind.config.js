fb3bb4e7cd60a27e35c26a296f698c47cd5f5538
egialaziz <egi.jack@gmail.com>
2025-06-15 11:01:54 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-
-// Trigger build
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  safelist: [
-    "border",
-    "border-orange-400",
-    "bg-orange-200",
-    "bg-orange-100",
-    "hover:bg-orange-50",
-    "rounded",
-    "shadow",
-    "overflow-x-auto"
-  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
