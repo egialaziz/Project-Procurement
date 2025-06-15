731b852c45e82351632781dc4ebbcd3035e6b925
egialaziz <egi.jack@gmail.com>
2025-06-15 10:18:33 +0700

    Update route.ts

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -2,7 +2,8 @@ import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
 export async function POST() {
-  const cookieStore = cookies();   // <-- Tambahan ini
+  const cookieStore = cookies();
   cookieStore.delete('sb-access-token');
+  cookieStore.delete('sb-refresh-token');
   return NextResponse.json({ success: true });
 }
