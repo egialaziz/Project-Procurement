9f6e73fee603119516a1b5d88fb23cdb4df01046
egialaziz <egi.jack@gmail.com>
2025-06-15 10:30:06 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    "border",
+    "border-orange-400",
+    "bg-orange-200",
+    "bg-orange-100",
+    "hover:bg-orange-50",
+    "rounded",
+    "shadow",
+    "overflow-x-auto"
+  ],
   theme: {
     extend: {},
   },
