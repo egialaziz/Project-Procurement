563a7c3e9d05b9ec62d52a886ad004054c20f0ee
egialaziz <egi.jack@gmail.com>
2025-06-15 20:18:31 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#f97316', // orange custom
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
