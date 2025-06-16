25d4eafdbab91b34e6d7c6bf9e2c5acb166b2afd
egialaziz <egi.jack@gmail.com>
2025-06-16 21:17:58 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    "w-[112px]",
+    "h-[112px]",
+    "w-28",
+    "h-28",
+    "w-16",
+    "h-16",
+  ],
   theme: {
     extend: {},
   },
