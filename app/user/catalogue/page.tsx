10f77bc5320f3d204f4232f0bd71435d7ecb252b
egialaziz <egi.jack@gmail.com>
2025-06-16 20:26:04 +0700

    Update page.tsx

--- app/user/catalogue/page.tsx
+++ app/user/catalogue/page.tsx
@@ -59,9 +59,13 @@ export default function UserCatalogue() {
           <tbody>
             {filteredData.map((row, idx) => (
               <tr key={idx} className="hover:bg-orange-100">
-                {Object.values(row).map((val, i) => (
-                  <td key={i} className="border border-orange-400 px-4 py-2">{val as any}</td>
-                ))}
+                {Object.entries(row).map(([key, val], i) => (
+  <td key={i} className="border border-orange-400 px-4 py-2">
+    {key === 'photo' && val
+      ? <img src={val as string} alt="Photo" className="w-20 h-20 object-cover rounded" />
+      : (val as any)}
+  </td>
+))}
               </tr>
             ))}
           </tbody>
