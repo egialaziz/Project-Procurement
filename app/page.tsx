d8538812ca2b20dadeccc0787269846ad7045ecc
egialaziz <egi.jack@gmail.com>
2025-06-24 09:06:13 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,14 @@ export default function Home() {
       
       {/* Text Section (Kiri) */}
       <section className="flex flex-col justify-center items-start p-10 lg:w-1/2 bg-[#fff5e6]">
-        <p className="text-sm text-gray-500 mb-2 uppercase tracking-wide">New Inventory</p>
-        <h1 className="text-4xl font-bold text-orange-600 mb-4">Procurement Catalogue</h1>
-        <p className="text-gray-700 mb-8">All procurement items listed clearly for user access.</p>
+        <p className="text-sm text-gray-500 mb-2 uppercase tracking-wide">Procurement Departement</p>
+        <h1 className="text-4xl font-bold text-orange-600 mb-4">e-Procurement Catalogue</h1>
+        <p className="text-gray-700 mb-8">Semua contoh dari barang permintaan model sendiri telah kami rangkum dalam katalog di situs ini.</p>
         <Link
           href="/user/catalogue"
           className="bg-black text-white py-3 px-6 rounded-full hover:bg-gray-800 transition"
         >
-          VIEW IT NOW
+          Lihat Katalog
         </Link>
       </section>
 
