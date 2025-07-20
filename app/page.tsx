15d2d0b173a75b7f9ae2ee7e6c61153480a8126e
egialaziz <egi.jack@gmail.com>
2025-07-20 12:19:53 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   <img
     src="/landing.jpg"
     alt="Background"
-    className="w-full h-full object-cover opacity-10 blur-sm scale-105"
+    className="w-full h-full object-cover opacity-50 blur-sm scale-105"
   />
   <div className="absolute inset-0 bg-black/60" />
 </div>
